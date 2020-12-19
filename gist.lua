3a80a25a171f42edbf650f2e65697f69898246a0
Winksplorer <oscar.botashev@gmail.com>
2020-12-18 16:37:25 -0800

    RELEASED GIST! 🚀

--- gist.lua
+++ gist.lua
@@ -1,6 +1,12 @@
-require=os
-
 gist = {}
+
+function gist.implement(moduel)
+	require(moduel)
+end
+
+gist.implement('os')
+gist.implement('modules.sys')
+gist.implement('modules.files')
 
 function gist.printTimeRunning()
 	print(os.clock())
@@ -14,21 +20,13 @@ function gist.consoleExec(cmdCommand)
 	os.execute(cmdCommand)
 end
 
-function gist.remove(file)
-	os.remove(file)
-end
-
-function gist.rename(newFilename, oldFilename)
-	os.rename(newFilename, oldFilename)
-end
-
 function gist.print(msg)
 	print(msg)
 end
 
 function gist.input(msgforinput)
-	io.write(msgforinput .. " ")
-	io.read()
+	io.write(msgforinput)
+	input = io.read()
 end
 
 function gist.exit(exitCode)
@@ -38,6 +36,15 @@ end
 function gist.sleep(duration)
 	local start = os.clock()
 	while os.clock() - start < duration do end
+
+	if(duration < 0) then
+		sys.createError(duration .. ' Is below 0', 'UnderflowError', 'gist.sleep(' .. duration .. ')', 'GIST', 1, true)
+		gist.exit(1)
+	end
+end
+
+function gist.clearScreen()
+	os.execute('cls')
 end
 
 return gist
